ce5ebfe9f1b87b3eb28139d92e5818414843d47d
Sudarshan Gowda <sudarshan.java.p@gmail.com>
2020-06-20 15:38:54 +0800

    Base Releas

---
+++ spring-boot-admin-client/src/main/java/com/star/sud/springbootadminclient/SpringBootAdminClientApplication.java
@@ -0,0 +1,13 @@
+package com.star.sud.springbootadminclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAdminClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminClientApplication.class, args);
+	}
+
+}
